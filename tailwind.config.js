824c26192dd3582a3015793b6c88bba3e608a039
Gustavo Souza <gustavo.s.silva651@gmail.com>
2023-06-20 22:17:16 -0300

    style: Including project color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,14 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+       colors: {
+        openai: {
+          200: '#11A37F',
+          500: '#343541',
+          600: '#434654',
+          800: '#202123'
+        }
+       }
     },
   },
   plugins: [],
