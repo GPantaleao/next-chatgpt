fb765fa5d598b771600cf3e74961a6c19d2576bf
Gustavo Souza <gustavo.s.silva651@gmail.com>
2023-07-11 14:52:10 -0300

    feat: Updating query function to use gpt-turbo-3.5

--- src/lib/queryApi.ts
+++ src/lib/queryApi.ts
@@ -1,20 +1,28 @@
 import openai from "./chatgpt";
 
 const query = async (prompt: string, chatId: string, model: string) => {
-  const response = await openai
-    .createCompletion({
-      model: `${model}`,
-      prompt: `${prompt}`,
-      temperature: 0.7,
-      top_p: 1,
-      max_tokens: 1000,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-    })
-    .then((res) => res.data.choices[0].text?.trim())
+  const response = await openai.createChatCompletion({
+    model: `${model}`,
+    messages: [
+      {
+        "role": "user",
+        "content": `${prompt}`
+      },
+      {
+        "role": "assistant",
+        "content": `${prompt}`
+      }
+    ],
+    temperature: 1,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  })
+    .then((res) => res.data.choices[0].message?.content?.trim())
     .catch(
-      (err) =>
-        `Chat GPT was unable to find an answer for that! (ERROR: ${err.message})`
+      (err) => 
+        console.log(err)
     );
 
   return response;
